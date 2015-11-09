a932e98b7593dec6ba2b9a42cff23b7aaea2f9a6
Elisabete Bicho <elisabete.bicho@forkit.pt>
2015-11-09 10:18:54 +0000

    Project setup

---
+++ TrackingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrackingApp
+//
+//  Created by Elisabete Sousa on 09/11/15.
+//  Copyright © 2015 Fork IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrackingApp/ViewControllers/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TrackingApp
+//
+//  Created by Elisabete Sousa on 09/11/15.
+//  Copyright © 2015 Fork IT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

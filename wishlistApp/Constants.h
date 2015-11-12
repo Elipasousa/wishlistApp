c55312b972df4d93ae1266e7d1ba91650cd4427b
Elisabete Bicho <elisabete.bicho@forkit.pt>
2015-11-12 13:22:11 +0000

    More brands added

--- wishlistApp/Constants.h
+++ wishlistApp/Constants.h
@@ -10,26 +10,65 @@
 #define Constants_h
 
 //Brands
+#define ADIDAS @"Adidas"
 #define ALDO @"Aldo"
 #define ASOS @"Asos"
+#define BENETTON @"Benetton"
 #define BERSHKA @"Bershka"
+#define BILLABONG @"Billabong"
 #define BIMBA_Y_LOLA @"Bimba Y Lola"
+#define BLANCO @"Blanco"
+#define CA @"C&A"
 #define CALZEDONIA @"Calzedonia"
+#define CLAIRES @"Claire's"
+#define CORTEFIEL @"Cortefiel"
+#define ELENAMIRO @"Elena Miró"
+#define ERICEIRASURFSHOP @"Ericeira Surf Shop"
+#define FOREVA @"Foreva"
+#define FURLA @"Furla"
+#define GANT @"Gant"
+#define GARDENIA @"Gardenia"
+#define GUESS @"Guess"
 #define H_M @"H&M"
+#define HOSSINTROPIA @"Hoss Intropia"
+#define HURLEY @"Hurley"
+#define HUSHPUPPIES @"Hush Puppies"
 #define INTIMISSIMI @"Intimissimi"
+#define KIKO @"Kiko"
+#define LANIDOR @"Lanidor"
+#define LEFTIES @"Lefties"
+#define LEVIS @"Levis"
+#define LONGCHAMP @"Longchamp"
 #define MANGO @"Mango"
 #define MASSIMO_DUTTI @"Massimo Dutti"
+#define MICHAELKORS @"Michael Kors"
+#define NATURA @"Natura"
 #define NEW_YORKER @"New Yorker"
+#define NIKE @"Nike"
+#define OYSHO @"Oysho"
+#define PARFOIS @"Parfois"
+#define PEPEJEANS @"Pepe Jeans"
+#define PIMKIE @"Pimkie"
 #define PRIMARK @"Primark"
+#define PROMOD @"Promod"
 #define PULL_AND_BEAR @"Pull and Bear"
+#define PURIFICACIONGARCIA @"Purificación García"
+#define QUEBRAMAR @"Quebramar"
+#define QUICKSILVER @"Quicksilver"
+#define ROCKPORT @"Rockport"
+#define SACOOR @"Sacoor"
+#define SALSA @"Salsa"
+#define SPRINGFIELD @"Springfield"
 #define STRADIVARIUS @"Stradivarius"
 #define TEZENIS @"Tezenis"
+#define TIFFOSI @"Tiffosi"
 #define UTERQUE @"Uterque"
+#define WOMENSECRET @"Women' Secret"
 #define ZARA @"Zara"
 
-#define BRAND_NAMES @[ALDO, ASOS, BERSHKA, BIMBA_Y_LOLA, CALZEDONIA, H_M, INTIMISSIMI, MANGO, MASSIMO_DUTTI, NEW_YORKER, PRIMARK, PULL_AND_BEAR, STRADIVARIUS, TEZENIS, UTERQUE, ZARA]
+#define BRAND_NAMES @[ADIDAS, ALDO, ASOS, BENETTON, BERSHKA, BILLABONG, BIMBA_Y_LOLA, BLANCO, CA, CALZEDONIA, CLAIRES, CORTEFIEL, ELENAMIRO, ERICEIRASURFSHOP, FOREVA, FURLA, GANT, GARDENIA, GUESS, H_M, HOSSINTROPIA, HURLEY, HUSHPUPPIES, INTIMISSIMI, KIKO, LANIDOR, LEFTIES, LEVIS, LONGCHAMP, MANGO, MASSIMO_DUTTI, MICHAELKORS, NATURA, NEW_YORKER, NIKE, OYSHO, PARFOIS, PEPEJEANS, PIMKIE, PRIMARK, PROMOD, PULL_AND_BEAR, PURIFICACIONGARCIA, QUEBRAMAR, QUICKSILVER, ROCKPORT, SACOOR, SALSA, SPRINGFIELD, STRADIVARIUS, TEZENIS, TIFFOSI, UTERQUE, WOMENSECRET, ZARA]
 
-#define BRAND_LOGOS @{ALDO : @"aldo", ASOS : @"asos", BERSHKA : @"bershka", BIMBA_Y_LOLA : @"bimbaylola", CALZEDONIA : @"calzedonia", H_M : @"hm", INTIMISSIMI : @"intimissimi", MANGO : @"mango", MASSIMO_DUTTI : @"massimodutti", NEW_YORKER : @"newyorker", PRIMARK : @"primark", PULL_AND_BEAR : @"pullandbear", STRADIVARIUS : @"stradivarius", TEZENIS : @"tezenis", UTERQUE : @"uterque", ZARA : @"zara"}
+#define BRAND_LOGOS @{ADIDAS : @"adidas", ALDO : @"aldo", ASOS : @"asos", BENETTON : @"benetton", BERSHKA : @"bershka", BILLABONG : @"billabong", BIMBA_Y_LOLA : @"bimbaylola", BLANCO : @"blanco", CA : @"ca", CALZEDONIA : @"calzedonia", CLAIRES : @"claires", CORTEFIEL : @"cortefiel", ELENAMIRO : @"elenamiro", ERICEIRASURFSHOP : @"ericeirasurfshop", FOREVA : @"foreva", FURLA : @"furla", GANT : @"gant", GARDENIA : @"gardenia", GUESS : @"guess", H_M : @"hm", HOSSINTROPIA : @"hossintropia", HURLEY : @"hurley", HUSHPUPPIES : @"hushpuppies", INTIMISSIMI : @"intimissimi", KIKO : @"kiko", LANIDOR : @"lanidor", LEFTIES : @"lefties", LEVIS : @"levies", LONGCHAMP : @"longchamp", MANGO : @"mango", MASSIMO_DUTTI : @"massimodutti", MICHAELKORS : @"michaelkors", NATURA : @"natura", NEW_YORKER : @"newyorker", NIKE : @"nike", OYSHO : @"oysho", PARFOIS : @"parfois", PEPEJEANS : @"pepejeans", PIMKIE : @"pimkie", PRIMARK : @"primark", PROMOD : @"promod", PULL_AND_BEAR : @"pullandbear", PURIFICACIONGARCIA : @"purificaciongarcia", QUEBRAMAR : @"quebramar", QUICKSILVER : @"quicksilver", ROCKPORT : @"rockport", SACOOR : @"sacoor", SALSA : @"salsa", SPRINGFIELD : @"springfield", STRADIVARIUS : @"stradivarius", TEZENIS : @"tezenis", TIFFOSI : @"tiffosi", UTERQUE : @"uterque", WOMENSECRET : @"womensecret", ZARA : @"zara"}
 
 
 //Colors
